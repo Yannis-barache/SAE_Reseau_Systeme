0a959955d5c5fb4332281f1cf2fb4419d72d1893
Yannis-barache <ybarache@icloud.com>
2023-12-06 23:49:40 +0100

    Connexion fonctionnelle

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
-    
+
+    final Socket clientSocket;
+    final BufferedReader in;
+
+    final PrintWriter out;
+    static final Scanner sc = new Scanner(System.in);// pour lire à partir du clavier
+
+    public Client(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+        try {
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void EnvoiMessage(String msg) {
+        out.println(msg);
+        out.flush();
+    }
+
+    public String ReceptionMessage() throws IOException {
+        String msg = in.readLine();
+        return msg;
+    }
+
+    public void Fermeture() throws IOException {
+        out.close();
+        clientSocket.close();
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 5000);
+            Client client = new Client(socket);
+            Thread envoyer = new Thread(new Runnable() {
+                String msg;
+                @Override
+                public void run() {
+                    while (true) {
+                        msg = sc.nextLine();
+                        client.EnvoiMessage(msg);
+                    }
+                }
+            });
+
+            Thread recevoir = new Thread(new Runnable() {
+                String msg;
+                @Override
+                public void run() {
+                    try {
+                        msg = client.ReceptionMessage();
+                        while (msg != null) {
+                            System.out.println(msg);
+                            msg = client.ReceptionMessage();
+                        }
+                        System.out.println("Serveur déconecté");
+                        // A la déconnexion du serveur, on ferme le client
+                        System.exit(0);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            envoyer.start();
+            recevoir.start();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
